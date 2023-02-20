fb31468f96c6fd202e21d7e4517eb950644f0841
Nick Turgeon <nturge@artic.edu>
2023-02-20 15:14:26 -0600

    imported node module and created public folder

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/',(req, res)=>{
+    app.SendFile('www/index.html')
+})
+app.listen(port, (err) =>{
+    if(err)return console.log(err)
+    console.log(`
+    Listening on http://localhost:${port}
+    Document root is ${__dirname}
+    Press Ctrl-C to quit.`)
+}) 
