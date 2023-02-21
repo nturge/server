609d6bc0f26bd1a9b4438727080fdf09653dcf3a
Nick Turgeon <nturge@artic.edu>
2023-02-21 12:10:54 -0600

    fingerprinting, building out rest api

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8000
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) =>  {
     res.sendFile(`${__dirname}/www/index.html`)
 })
-app.listen(port, (err) =>{
+
+app.post('/api/fingerprint', (req, res) => {
+    console.log(req.body)
+})
+app.listen(port, (err) => {
     if(err)return console.log(err)
     console.log(`
     Listening on http://localhost:${port}
