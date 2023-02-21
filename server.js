b553650ec7c20b09d0e9054518f9f8732822e47f
Nick Turgeon <nturge@artic.edu>
2023-02-21 14:48:05 -0600

    added database as json file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const crypto = require('crypto')
+const fs = require('fs')
+const db = require('./database.json')
 const port = 8000
 
 app.use(bodyParser.json())
@@ -9,8 +12,28 @@ app.get('/', (req, res) =>  {
     res.sendFile(`${__dirname}/www/index.html`)
 })
 
+function updateDB (){
+    const data = JSON.stringify(db)
+    fs.writeFile('./database.json', data, () =>{
+    if (err) console.log('something went wrong', err)
+    else console.log('database was updated!')    
+    })
+}
+
 app.post('/api/fingerprint', (req, res) => {
-    console.log(req.body)
+    const md5 = crypto.createHash('md5')
+    const str = JSON.stringify(req.body)
+    const bfp = md5.update(str, 'utf8').digest('hex')
+    console.log(bfp)
+
+    if (db.hasOwnProperty(bfp)){
+        db[bfp].push(Date.now())
+    }else {
+        db[bfp]= [Date.now()]
+    }
+
+    updateDB()
+
     res.json({message: 'got it!' })
 })
 app.listen(port, (err) => {
